2c577ccc76e6bdba65c75e988f60a13a2247d382
Vladimir Farafonov <vfarafonov@weezlabs.com>
2015-10-05 09:44:48 +0300

    Implemented font picker

--- app/src/main/java/com/weezlabs/soapeditor/MainActivity.java
+++ app/src/main/java/com/weezlabs/soapeditor/MainActivity.java
@@ -1,11 +1,14 @@
 package com.weezlabs.soapeditor;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
@@ -19,6 +22,16 @@ public class MainActivity extends AppCompatActivity {
 	private View contentView_;
 	private EditText overlayTextView_;
 	private ColorPicker colorPicker_;
+	private FontPicker fontPicker_;
+
+	View.OnTouchListener pickersTouchListener_ = new View.OnTouchListener() {
+		@Override
+		public boolean onTouch(View v, MotionEvent event) {
+			v.requestFocus();
+			hideKeyboard(v.getWindowToken());
+			return false;
+		}
+	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +40,22 @@ public class MainActivity extends AppCompatActivity {
 		rootView_ = (RelativeLayout) findViewById(R.id.root_layout);
 		contentView_ = findViewById(R.id.iv_background_image);
 		colorPicker_ = (ColorPicker) findViewById(R.id.color_picker);
+		colorPicker_.setOnTouchListener(pickersTouchListener_);
 		colorPicker_.setColorChangedListener(new ColorPicker.ColorChangedListener() {
 			@Override
 			public void onColorChanged(int color) {
 				if (overlayTextView_ != null) {
 					overlayTextView_.setTextColor(color);
+				}
+			}
+		});
+		fontPicker_ = (FontPicker) findViewById(R.id.font_picker);
+		fontPicker_.setOnTouchListener(pickersTouchListener_);
+		fontPicker_.setFontChangedListener(new FontPicker.FontChangedListener() {
+			@Override
+			public void onFontChanged(Typeface typeface) {
+				if (overlayTextView_ != null){
+					overlayTextView_.setTypeface(typeface);
 				}
 			}
 		});
@@ -53,14 +77,21 @@ public class MainActivity extends AppCompatActivity {
 		changeTextButton.setOnClickListener(changeTextClickListener_);
 	}
 
+	private void hideKeyboard(IBinder windowToken) {
+		InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+		inputMethodManager.hideSoftInputFromWindow(windowToken, 0);
+	}
+
 	private void initiateOverlayTextView() {
 		overlayTextView_ = new EditText(getBaseContext());
 		overlayTextView_.setTextSize(50);
+		overlayTextView_.setBackground(null);
 		overlayTextView_.setText("asdasda");
 		overlayTextView_.setGravity(Gravity.CENTER);
 		overlayTextView_.setWidth(contentView_.getWidth());
 		overlayTextView_.setHeight(contentView_.getHeight());
 		overlayTextView_.setTextColor(colorPicker_.getSelectedColor());
+		overlayTextView_.setTypeface(fontPicker_.getSelectedFont());
 		RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) contentView_.getLayoutParams();
 		overlayTextView_.setLayoutParams(layoutParams);
 		rootView_.addView(overlayTextView_);
