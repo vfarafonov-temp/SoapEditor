5a35c7f88c247501fc41bbc751d5ae249db988b6
Vladimir Farafonov <vfarafonov@weezlabs.com>
2015-10-12 16:10:23 +0300

    Create TextToolsView for displaying text editing layout and handling editing actions

--- app/src/main/java/com/weezlabs/soapeditor/TextMessageActivity.java
+++ app/src/main/java/com/weezlabs/soapeditor/TextMessageActivity.java
@@ -1,13 +1,18 @@
 package com.weezlabs.soapeditor;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+import com.weezlabs.soapeditor.views.TextToolsView;
 
 public class TextMessageActivity extends AppCompatActivity {
+
+	private TextToolsView textTools_;
+	private Menu menu_;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +21,49 @@ public class TextMessageActivity extends AppCompatActivity {
 		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+		textTools_ = (TextToolsView) findViewById(R.id.text_tools);
+		EditText textField_ = (EditText) findViewById(R.id.et_message_field);
+		textTools_.setEditableView(textField_);
+		textTools_.setTextToolsListener(new TextToolsView.TextToolsListener() {
+			@Override
+			public void onTextAlignmentChanged(int newGravity) {
+
+			}
+
+			@Override
+			public void onModeChanged(TextToolsView.EditingModes newMode, TextToolsView.EditingModes oldMode) {
+				switch (newMode) {
+					case BACKGROUND_COLOR_CHANGING:
+						menu_.findItem(R.id.action_done).setVisible(true);
+						break;
+					case FONT_CHANGING:
+						menu_.findItem(R.id.action_done).setVisible(true);
+						break;
+					case TEXT_EDITING:
+						menu_.findItem(R.id.action_done).setVisible(false);
+						break;
+				}
+			}
+		});
 	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.menu_text_message, menu);
+		menu_ = menu;
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.action_done:
+				textTools_.setMode(TextToolsView.EditingModes.TEXT_EDITING);
+				menu_.findItem(R.id.action_done).setVisible(false);
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+	}
 }
